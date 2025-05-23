6ca7f1975e401783c223e6ea445aceba5861aecc
Flávia Reis <flaviarster@gmail.com>
2025-05-23 08:31:04 -0300

    fix: corrige path

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import { app } from "./app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor tá ON em http://localhost/${PORT}`);
+  console.log(`Servidor tá ON em http://localhost:${PORT}`);
 });
