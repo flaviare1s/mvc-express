2aa43785f5696d7564472a7c0ee3f36abacf6350
Flávia Reis <flaviarster@gmail.com>
2025-05-27 11:59:38 -0300

    feat: adiciona model endereco

---
+++ src/models/endereco.model.js
@@ -0,0 +1,12 @@
+class Endereco {
+  constructor({ rua, numero, cidade, estado, cep, complemento }) {
+    this.rua = rua;
+    this.numero = numero;
+    this.cidade = cidade;
+    this.estado = estado;
+    this.cep = cep;
+    this.complemento = complemento;
+  }
+}
+
+export default Endereco;
